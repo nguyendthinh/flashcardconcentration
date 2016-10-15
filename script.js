61dfb602cdc8628ae2695588fc34e1712690b310
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-15 13:44:38 -0400

    fixed a few kinks

--- script.js
+++ script.js
@@ -34,12 +34,7 @@ $(document).ready(function(){
 
   function lightSequence(){
     randomSequence = posSequence[Math.floor(Math.random() * posSequence.length)];
-    // console.log(storedSequences + "%");
     storedSequences.push(randomSequence.attr("id"));
-    // $(randomSequence).fadeIn(400).fadeOut(400).fadeIn(400);
-    // console.log(sequenceColors);
-    // console.log(storedSequences + "!");
-    console.log(storedSequences + "$");
     setTimeout(function() { showColors(0);}, 500);
   }
 
@@ -47,7 +42,6 @@ function showColors(index){
   if (index < storedSequences.length) {
     $('#' + storedSequences[index]).fadeIn(400).fadeOut(400).fadeIn(400);
     setTimeout(function() { showColors(index + 1);}, 700);
-    console.log(storedSequences[index]);
   }
 }
 
@@ -57,21 +51,42 @@ function showColors(index){
     $(this).fadeIn(400).fadeOut(400).fadeIn(400);
     playerClicks.push($(this).attr("id"));
 
-    for (var i=0; i < storedSequences.length; i++) {
-      if (playerClicks.length === storedSequences.length) {
-        if (playerClicks[i] === storedSequences[i]) {
-        playerClicks = [];
-        console.log(storedSequences +"?");
-        // alert("Move on to the next round");
-        return;
-      } else {
-        // alert("You don't got it");
-        playerClicks = [];
-        storedSequences = [];
-        return;
-      }
+    if (playerClicks.length === storedSequences.length) {
+      storedSequences.every(function(element, index){
+        return element === playerClicks[index]
+      }) ? nextRound() : startAgain();
     }
-  }
+
+    function nextRound() {
+      playerClicks = [];
+      alert("you got it");
+      console.log(storedSequences);
+    }
+
+    function startAgain() {
+      playerClicks = [];
+      storedSequences = [];
+      alert("try again");
+    }
+
+  //  for (var i=0; i < storedSequences.length; i++) {
+  //     if (playerClicks.length === storedSequences.length) {
+  //       if (playerClicks === storedSequences) {
+  //       console.log(playerClicks);
+  //       console.log(storedSequences);
+  //       playerClicks = [];
+  //       alert("you got it");
+  //     } else {
+  //       console.log(playerClicks);
+  //       console.log(storedSequences);
+  //       alert("try again");
+  //       playerClicks = [];
+  //       storedSequences = [];
+  //     }
+  //   }
+  // }
+
+//bracket closes divClick
   }
 
 });
